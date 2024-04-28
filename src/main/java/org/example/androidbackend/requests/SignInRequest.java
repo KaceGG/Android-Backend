c80f59cc4dbfd169d777e52a862a4e6909373591
KaceGG <ndangkhoa567@gmail.com>
2024-04-28 18:23:22 +0700

    change something

--- src/main/java/org/example/androidbackend/requests/SignInRequest.java
+++ src/main/java/org/example/androidbackend/requests/SignInRequest.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignInRequest {
-    private String username ;
+    private String username;
     private String password;
 }
